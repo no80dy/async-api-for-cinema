2119eef14a15d16ef634933d1ec6c6c2e4525fd3
Ruslan Rakhmatullin <RakhmatullinRR@tatneft.tatar>
2023-10-21 21:20:20 +0300

    разделил модели по отдельным файлам

---
+++ src/models/genre.py
@@ -0,0 +1,10 @@
+from models.film import BaseModelOrjson
+
+
+class Genres(BaseModelOrjson):
+    """
+    Схемы ответов для:
+    /api/v1/genres/
+    /api/v1/genres/<uuid:UUID>/
+    """
+    name: str
