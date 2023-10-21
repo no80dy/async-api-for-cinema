c350ed18b706acdc0830eb4d2658e68d6808c4fc
no80dy <no8odyas@gmail.com>
2023-10-21 23:57:44 +0300

    Добавлена функциональность вывода информации о кинопроизведениях в отсортированном виде, а также в отфильтрованном по конкретному жанру

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,38 +1,18 @@
 from uuid import UUID
-from typing import List
+from typing import List, Optional
 from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException, Query
 
 from services.film import FilmService, get_film_service
-from models.film import Film
+from models.film import FilmShort
 
 
 router = APIRouter()
 
 
 @router.get(
-    '/{film_id}',
-    response_model=Film,
-    summary="Поиск кинопроизведений",
-    description="Полнотекстовый поиск по кинопроизведениям",
-    response_description="Название и рейтинг фильма",
-)
-async def film_details(
-    film_id: UUID,
-    film_service: FilmService = Depends(get_film_service)
-) -> Film:
-    film = await film_service.get_by_id(str(film_id))
-
-    if not film:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='film not found'
-        )
-    return Film(id=film.id, title=film.title)
-
-
-@router.get(
     '/search',
-    response_model=List[Film],
+    response_model=List[FilmShort],
     summary="Поиск кинопроизведений",
     description="Полнотекстовый поиск по кинопроизведениям",
     response_description="Название и рейтинг филкинопроизведения",
@@ -42,13 +22,72 @@ async def search_film(
     page_size: int = Query(..., ge=1),
     page_number: int = Query(..., ge=1),
     film_service: FilmService = Depends(get_film_service)
-) -> List[Film]:
+) -> List[FilmShort]:
     films = await film_service.get_films_by_query(
         query, page_size, page_number
     )
 
     if not films:
         raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='films not found'
+        )
+
+    return [
+        FilmShort(id=film.id, title=film.title, imdb_rating=film.imdb_rating)
+        for film in films
+    ]
+
+
+@router.get(
+    '/{film_id}',
+    response_model=FilmShort,
+    summary="Поиск кинопроизведений",
+    description="Полнотекстовый поиск по кинопроизведениям",
+    response_description="Название и рейтинг фильма",
+)
+async def film_details(
+    film_id: str,
+    film_service: FilmService = Depends(get_film_service)
+) -> FilmShort:
+    film = await film_service.get_film_by_id(str(film_id))
+
+    if not film:
+        raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='film not found'
         )
-    return films
+    return FilmShort(
+        id=film.id, title=film.title, imdb_rating=film.imdb_rating
+    )
+
+
+@router.get(
+    '/',
+    response_model=List[FilmShort],
+    summary="Поиск кинопроизведений",
+    description="Полнотекстовый поиск по кинопроизведениям",
+    response_description="Название и рейтинг фильма",
+)
+async def films(
+    genre_id: Optional[UUID] = None,
+    sort: str = Query('-imdb_rating'),
+    page_size: int = Query(..., ge=1),
+    page_number: int = Query(..., ge=1),
+    film_service: FilmService = Depends(get_film_service)
+) -> List[FilmShort]:
+    if genre_id:
+        films = await film_service.get_ilms_by_genre_id_with_sort(
+            genre_id, sort, page_size, page_number
+        )
+    else:
+        films = await film_service.get_films_with_sort(
+            sort, page_size, page_number
+        )
+
+    if not films:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='films not found'
+        )
+    return [
+        FilmShort(id=film.id, title=film.title, imdb_rating=film.imdb_rating)
+        for film in films
+    ]
