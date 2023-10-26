06f7ccc4e4bdddde87f13dc1109c47e8244919fc
no80dy <no8odyas@gmail.com>
2023-10-26 11:10:19 +0300

    Заменил устаревший on_event на lifespan в модуле main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,42 @@
 import uvicorn
+from redis.asyncio import Redis
 from elasticsearch import AsyncElasticsearch
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from redis.asyncio import Redis
 
-from api.v1 import films, genres, persons
-from api.v1 import films, genres
 from core import config
 from db import elastic, redis
+from api.v1 import films, genres, persons
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Подключаемся к базам данных при включении сервера
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(
+        hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    yield
+    # Отключаемся от баз при выключении сервера
+    await redis.redis.close()
+    await elastic.es.close()
 
 
 app = FastAPI(
-    description="Информация о фильмах, жанрах и людях, участвовавших в создании произведения",
-    version="1.0.0",
+    description='Информация о фильмах, жанрах и людях, участвовавших в создании произведения',
+    version='1.0.0',
     # Конфигурируем название проекта. Оно будет отображаться в документации
     title=config.PROJECT_NAME,
     # Адрес документации в красивом интерфейсе
     docs_url='/api/openapi',
     # Адрес документации в формате OpenAPI
     openapi_url='/api/openapi.json',
-    # Можно сразу сделать небольшую оптимизацию сервиса
-    # и заменить стандартный JSON-сереализатор на более шуструю версию, написанную на Rust
     default_response_class=JSONResponse,
+    lifespan=lifespan
 )
 
 
-@app.on_event('startup')
-async def startup():
-    # Подключаемся к базам при старте сервера
-    # Подключиться можем при работающем event-loop
-    # Поэтому логика подключения происходит в асинхронной функции
-    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
-    elastic.es = AsyncElasticsearch(
-        hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    # Отключаемся от баз при выключении сервера
-    await redis.redis.close()
-    await elastic.es.close()
-
-
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
@@ -51,9 +44,7 @@ app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 
 if __name__ == '__main__':
     # Приложение может запускаться командой
-    # `uvicorn main:app --host 0.0.0.0 --port 8000`
-    # но чтобы не терять возможность использовать дебагер,
-    # запустим uvicorn сервер через python
+    # `uvicorn main:app --host 0.0.0.0 --port 8000 --reload`
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
