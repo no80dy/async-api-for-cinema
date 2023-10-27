7742cdcca8c2342eecef0af718fea9a6e03ad5ea
no80dy <no8odyas@gmail.com>
2023-10-27 20:57:23 +0300

    Оформил модуль config.py в виде pydantic класса Settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,21 +1,23 @@
 import os
+
+from pydantic import Field
+from core.logger import LOGGING
+from pydantic_settings import BaseSettings
 from logging import config as logging_config
 
-from core.logger import LOGGING
+
+class Settings(BaseSettings):
+    project_name: str = Field('movies')
+    redis_host: str = Field('127.0.0.1')
+    redis_port: int = Field(6379)
+    es_host: str = Field('127.0.0.1')
+    es_port: int = Field(9200)
+
+
+settings = Settings()
 
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
-
-# Название проекта. Используется в Swagger-документации
-PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
-
-# Настройки Redis
-REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-
-# Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ES_HOST', '127.0.0.1')
-ELASTIC_PORT = int(os.getenv('ES_PORT', 9200))
 
 # Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
