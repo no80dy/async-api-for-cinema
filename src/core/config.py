bc710b9f9acf116ef8050ba1d6c406279aed3f27
no80dy <no8odyas@gmail.com>
2023-10-27 21:00:55 +0300

    Убрал Field из класса Settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,17 +1,16 @@
 import os
 
-from pydantic import Field
 from core.logger import LOGGING
 from pydantic_settings import BaseSettings
 from logging import config as logging_config
 
 
 class Settings(BaseSettings):
-    project_name: str = Field('movies')
-    redis_host: str = Field('127.0.0.1')
-    redis_port: int = Field(6379)
-    es_host: str = Field('127.0.0.1')
-    es_port: int = Field(9200)
+    project_name: str = 'movies'
+    redis_host: str = '127.0.0.1'
+    redis_port: int = 6379
+    es_host: str = '127.0.0.1'
+    es_port: int = 9200
 
 
 settings = Settings()
