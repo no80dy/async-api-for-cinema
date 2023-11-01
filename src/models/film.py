e99048baf9b5cb3da85ad58d23af343c5cc1a137
no80dy <no8odyas@gmail.com>
2023-11-01 15:29:43 +0300

    Исправлены поля pydantic классов в модуле models/film.py

--- src/models/film.py
+++ src/models/film.py
@@ -34,8 +34,8 @@ class Film(FilmShort):
     /api/v1/films/<uuid:UUID>/
     """
     title: str
-    imdb_rating: float
-    description: str
+    imdb_rating: float | None
+    description: str | None
     genres: list[Genres] | None = None
     actors: list[IdName] | None = None
     writers: list[IdName] | None = None
