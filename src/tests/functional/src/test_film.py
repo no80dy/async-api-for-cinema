41a810a66b668ba5bf0018bdc589757a12e2f525
no80dy <no8odyas@gmail.com>
2023-11-10 12:26:57 +0300

    Произведен рефакторинг тестов для кинопроизведений

--- src/tests/functional/src/test_film.py
+++ src/tests/functional/src/test_film.py
@@ -6,21 +6,62 @@ from ..settings import test_settings
 from ..testdata.es_data import es_films_data
 
 
+FILMS_RESPONSE_DATA = [
+    {
+    'uuid': es_film_data['id'],
+    'title': es_film_data['title'],
+    'imdb_rating': es_film_data['imdb_rating'],
+    'description': es_film_data['description'],
+    'genres': [{
+        'uuid': genre['id'],
+        'name': genre['name']
+    } for genre in es_film_data['genres']],
+    'actors': [{
+        'id': actor['id'],
+        'name': actor['name']
+    } for actor in es_film_data['actors']],
+    'writers': [{
+        'id': writer['id'],
+        'name': writer['name']
+    } for writer in es_film_data['writers']],
+    'directors': [{
+        'id': director['id'],
+        'name': director['name']
+    } for director in es_film_data['directors']],
+} for es_film_data in es_films_data]
+
+
+FILMS_SHORT_RESPONSE_DATA = [
+    {
+        'uuid': es_film_data['id'],
+        'title': es_film_data['title'],
+        'imdb_rating': es_film_data['imdb_rating']
+    } for es_film_data in es_films_data]
+
+HTTP_200 = 200
+HTTP_404 = 404
+HTTP_422 = 422
+
+
 @pytest.mark.parametrize(
     'film_data, expected_answer',
     [
         (
             {'film_id': es_films_data[0]['id']},
-            {'status': 200, 'body': es_films_data[0]}
+            {'status': HTTP_200, 'body': FILMS_RESPONSE_DATA[0]}
+        ),
+        (
+            {'film_id': es_films_data[1]['id']},
+            {'status': HTTP_200, 'body': FILMS_RESPONSE_DATA[1]}
         ),
         (
             {'film_id': str(uuid.uuid4())},
-            {'status': 404, 'body': { 'detail': 'films not found' } }
+            {'status': HTTP_404, 'body': { 'detail': 'films not found' } }
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_search_film_by_film_id(
+async def test_search_film_by_film_id_positive(
     make_get_request,
     es_write_data,
     film_data,
@@ -31,7 +72,6 @@ async def test_search_film_by_film_id(
     film_id = film_data.get('film_id')
     response = await make_get_request(f'films/{film_id}', {})
 
-    expected_answer['uuid'] = expected_answer.pop('id')
     assert response.get('status') == expected_answer.get('status')
     assert dict(response.get('body')) == expected_answer.get('body')
 
@@ -40,13 +80,17 @@ async def test_search_film_by_film_id(
     'film_data, expected_answer',
     [
         (
-            {'film_id': 'wrong_uuid'},
+            {'film_id': 'string'},
+            {'status': 422}
+        ),
+        (
+            {'film_id': 0},
             {'status': 422}
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_search_film_by_film_id(
+async def test_search_film_by_film_id_negative(
     make_get_request,
     es_write_data,
     film_data,
@@ -65,16 +109,16 @@ async def test_search_film_by_film_id(
     [
         (
             {'page_size': 10, 'page_number': 1},
-            {'status': 200, 'length': 10}
+            {'status': HTTP_200, 'length': 10}
         ),
         (
             {'page_size': 100, 'page_number': 1},
-            {'status': 200, 'length': 50}
+            {'status': HTTP_200, 'length': 50}
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_films_pagination(
+async def test_films_pagination_positive(
     make_get_request,
     es_write_data,
     film_data,
@@ -92,17 +136,17 @@ async def test_films_pagination(
     'film_data, expected_answer',
     [
         (
-            {'page_size': -10, 'page_number': -1},
-            {'status': 422}
+            {'page_size': -1, 'page_number': -1},
+            {'status': HTTP_422}
         ),
         (
-            {'page_size': 'wrong', 'page_number': 'wrong'},
-            {'status': 422}
+            {'page_size': 'string', 'page_number': 'string'},
+            {'status': HTTP_422}
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_wrong_films_pagination(
+async def test_films_pagination_negative(
     make_get_request,
     es_write_data,
     film_data,
@@ -120,16 +164,16 @@ async def test_wrong_films_pagination(
     [
         (
             {'genre_id': es_films_data[0]['genres'][0]['id']},
-            {'status': 200}
+            {'status': HTTP_200, 'body': FILMS_SHORT_RESPONSE_DATA[:1]}
         ),
         (
-            {'genre_id': str(uuid.uuid4())},
-            {'status': 404}
-        )
+            {'genre_id': es_films_data[1]['genres'][0]['id']},
+            {'status': HTTP_200, 'body': FILMS_SHORT_RESPONSE_DATA[1:2]}
+        ),
     ]
 )
 @pytest.mark.asyncio
-async def test_search_films_by_genre_id(
+async def test_get_films_by_genre_id_positive(
     make_get_request,
     es_write_data,
     film_data,
@@ -141,20 +185,24 @@ async def test_search_films_by_genre_id(
     response = await make_get_request('films/', {'genre_id': genre_id})
 
     assert response.get('status') == expected_answer.get('status')
-    assert isinstance(response.get('body'), list)
+    assert response.get('body') == expected_answer.get('body')
 
 
 @pytest.mark.parametrize(
     'film_data, expected_answer',
     [
         (
-            {'genre_id': 'wrong'},
-            {'status': 422}
+            {'genre_id': 'string'},
+            {'status': HTTP_422}
+        ),
+        (
+            {'genre_id': 0},
+            {'status': HTTP_422}
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_search_films_by_genre_id(
+async def test_get_films_by_genre_id_negative(
     make_get_request,
     es_write_data,
     film_data,
--- src/tests/functional/testdata/es_data.py
+++ src/tests/functional/testdata/es_data.py
@@ -1,17 +1,17 @@
 import uuid
+import datetime
 
 
 DATA_ROWS = 50
 
 es_films_data = [{
         'id': str(uuid.uuid4()),
+        'imdb_rating': 8.5,
+        'genres': [{'id': str(uuid.uuid4()), 'name': 'Action'}],
         'title': 'The Star',
         'description': 'New World',
-        'imdb_rating': 8.5,
-        'genres': [
-            {'id': str(uuid.uuid4()), 'name': 'Action'},
-            {'id': str(uuid.uuid4()), 'name': 'Drama'}
-        ],
+        'actors_names': 'Ann',
+        'writers_names': 'Ben',
         'directors': [
             {'id': str(uuid.uuid4()), 'name': 'Ann'},
             {'id': str(uuid.uuid4()), 'name': 'Bob'}
@@ -24,8 +24,13 @@ es_films_data = [{
             {'id': str(uuid.uuid4()), 'name': 'Ben'},
             {'id': str(uuid.uuid4()), 'name': 'Howard'}
         ],
-    }
-    for _ in range(DATA_ROWS)]
+        'creation_date': datetime.datetime.now().isoformat(),
+
+        'type': 'type',
+        'genres_names': 'genres_names',
+        'directors_names': 'directors_names',
+
+    } for _ in range(DATA_ROWS)]
 
 
 es_persons_data = [{
