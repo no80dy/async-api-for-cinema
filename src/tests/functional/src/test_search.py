bf5099a0071691ba7287f935190426835e1b5c21
Ruslan Rakhmatullin <RakhmatullinRR@tatneft.tatar>
2023-11-11 09:54:06 +0300

    тесты search на проверку кэша

--- src/tests/functional/src/test_search.py
+++ src/tests/functional/src/test_search.py
@@ -1,4 +1,7 @@
+import json
+
 import pytest
+from redis.asyncio import Redis
 
 from ..settings import test_settings
 from ..testdata.es_data import es_films_data, es_persons_data
@@ -247,3 +250,43 @@ async def test_search_films_negative(make_get_request, es_write_data, query_data
 
     # 4. Проверяем ответ
     assert response.get('status') == expected_http_code, 'при невалидных значениях, получен ответ отличный от 422'
+
+
+@pytest.mark.parametrize(
+    'query_data, expected_answer, endpoint, data, index',
+    [
+        # дефолтная пагинация
+        (
+                {'query': 'Star', 'page_size': 10, 'page_number': 1},
+                {'status': HTTP_200, 'length': 50},
+                'films/search',
+                es_films_data,
+                test_settings.es_movies_index,
+        ),
+        (
+                {'query': 'Mat', 'page_size': 10, 'page_number': 1},
+                {'status': HTTP_200, 'length': 50},
+                'persons/search',
+                es_persons_data,
+                test_settings.es_persons_index,
+        ),
+    ]
+)
+@pytest.mark.asyncio
+async def test_search_with_cache(
+        redis_client, make_get_request, es_write_data, query_data, expected_answer, endpoint, data, index
+):
+    key_string = f'{query_data.get("query")}/{query_data.get("page_size")}/{query_data.get("page_number")}'
+    key = bytes(key_string, 'utf-8')
+    redis_client.set(key, '')
+
+    await make_get_request(endpoint, query_data)
+
+    value = redis_client.get(key)
+    str1 = value.decode("UTF-8")
+    dict_str = json.loads(str1)
+    list_of_dicts = [json.loads(s) for s in dict_str]
+
+    assert (
+            list_of_dicts == data[:query_data.get("page_size")]
+    ), 'В кэше значения после вызова эндпоинта search не соответствуют ожидаемым'
