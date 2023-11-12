180ef5f281e56c915415f2b7bd4971fe451e7be6
no80dy <no8odyas@gmail.com>
2023-11-13 00:33:11 +0300

    Убрал print и поставил logger

--- src/tests/functional/utils/wait_for_es.py
+++ src/tests/functional/utils/wait_for_es.py
@@ -1,6 +1,13 @@
 import time
+import sys
+from pathlib import Path
+import logging
 
 from elasticsearch import Elasticsearch
+
+sys.path.append(str(Path(__file__).resolve().parents[3]))
+
+from tests.functional.logger import logger
 
 HOST = 'elastic'
 
@@ -8,6 +15,6 @@ HOST = 'elastic'
 es_client = Elasticsearch(hosts=f'{HOST}:9200')
 while True:
     if es_client.ping():
-        print('Elasticsearch ping Ok')
+        logger.info('Elasticsearch ping Ok', )
         break
     time.sleep(1)
--- src/tests/functional/utils/wait_for_redis.py
+++ src/tests/functional/utils/wait_for_redis.py
@@ -1,6 +1,13 @@
 import time
-
+import sys
 import redis
+import logging
+
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[3]))
+
+from tests.functional.logger import logger
 
 
 HOST = 'redis'
@@ -8,7 +15,6 @@ HOST = 'redis'
 r = redis.Redis(host=HOST)
 while True:
     if r.ping():
-
-        print('Redis ping Ok')
+        logger.info('Redis ping Ok')
         break
     time.sleep(1)
