821e78a2c11fabb8624993db47320a36eb171243
sergei iakimchuk <si32cs50@gmail.com>
2023-11-13 14:18:52 +0300

    Добавлен backoff для полдключения к редис и эластик

--- src/tests/functional/utils/wait_for_es.py
+++ src/tests/functional/utils/wait_for_es.py
@@ -1,13 +1,30 @@
 import time
 
+import backoff
+import requests
+
 from elasticsearch import Elasticsearch
 
+
 HOST = 'elastic'
+PORT = 9200
+BACKOFF_MAX_TIME = 60
 
 
-es_client = Elasticsearch(hosts=f'{HOST}:9200')
-while True:
-    if es_client.ping():
-        print('Elasticsearch ping Ok')
-        break
-    time.sleep(1)
+if __name__ == '__main__':
+    es_client = Elasticsearch(hosts=f'{HOST}:{PORT}')
+
+    @backoff.on_exception(backoff.expo, (requests.exceptions.Timeout,
+                                         requests.exceptions.ConnectionError), max_time=BACKOFF_MAX_TIME)
+    def check_es_readiness():
+        while True:
+            if es_client.ping():
+                print('Elasticsearch ping Ok')
+                break
+            time.sleep(1)
+
+    try:
+        check_es_readiness()
+    except ConnectionError:
+        print('Elasticsearch is not available')
+        raise ConnectionError
